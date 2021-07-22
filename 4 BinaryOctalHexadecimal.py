009e4a8a2f9105964408f6362719168923004fa5
alokkjnu <ry7069577@gmail.com>
2021-07-22 15:34:23 +0530

    commit

---
+++ 4 BinaryOctalHexadecimal.py
@@ -0,0 +1,21 @@
+# Working with Binary, Octal and Hexadecimal Integers
+
+x = 1234
+print(bin(x))
+print(oct(x))
+print(hex(x))
+
+print(format(x,'b'))
+print(format(x,'o'))
+print(format(x,'x'))
+
+x = -1234
+print(format(x,'b'))
+print(format(x,'o'))
+print(format(x,'x'))
+print(format(2**32 + x,'b'))
+print(format(2**32 + x,'x'))
+print(format(2**32 + x,'o'))
+
+print(int('4d2', 16))
+print(int('10011010010', 2))
