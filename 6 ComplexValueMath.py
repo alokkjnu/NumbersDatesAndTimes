ebc28527fc478787387211be30fa532d6ff9fe65
alokkjnu <ry7069577@gmail.com>
2021-07-22 18:28:36 +0530

    commit

---
+++ 6 ComplexValueMath.py
@@ -0,0 +1,27 @@
+# Performing Complex Value Math
+
+a = complex( 2, 4 )
+b = 3 - 5j
+print(a)
+print(b)
+
+print(a.real)
+print(a.imag)
+print(a.conjugate())
+
+print(a+b)
+print(a*b)
+print(a/b)
+print(abs(a))
+
+import cmath
+print(cmath.sin(a))
+print(cmath.cos(a))
+print(cmath.tan(a))
+print(cmath.exp(a))
+
+import numpy as np
+a = np.array([2+3j,4+5j,6-7j,8+9j])
+print(a)
+print(a+2)
+print(np.sin(a))
